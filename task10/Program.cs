ed8333534b5c067961663375acbfb991201985cd
maks <maks-lapin-1998@.ru>
2023-04-30 21:14:20 +0400

    task10

---
+++ task10/Program.cs
@@ -0,0 +1,7 @@
+﻿System.Console.WriteLine("Введите число");
+string a = Console.ReadLine();
+int a1 = int.Parse(a);
+
+int second = a1 / 10 % 10;
+
+System.Console.WriteLine($"Вторая цифра числа {a} - это {second}.");
